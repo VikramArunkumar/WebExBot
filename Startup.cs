9ba69ac4f9f4fd17b0bf4703dfd5337f64957101
VikramArunkumar <bravevik345@gmail.com>
2021-06-08 10:21:32 -0400

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// This method gets called by the runtime. Use this method to add services to the container.
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllers().AddNewtonsoftJson();
+
+    // Create the default Bot Framework Adapter.
+    services.AddSingleton<IBotFrameworkHttpAdapter, BotFrameworkAdapterWithErrorHandler>();
+
+    // Create the default Bot Framework Adapter.
+    services.AddSingleton<WebexAdapter, WebexAdapterWithErrorHandler>();
+
+    // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+    services.AddTransient<IBot, EchoBot>();
+}
---
+++ WebexController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+[Route("api/webex")]
+[ApiController]
+public class WebexController : ControllerBase
+{
+    private readonly WebexAdapter _adapter;
+    private readonly IBot _bot;
+
+    public WebexController(WebexAdapter adapter, IBot bot)
+    {
+        _adapter = adapter;
+        _bot = bot;
+    }
+
+    [HttpPost]
+    public async Task PostAsync()
+    {
+        // Delegate the processing of the HTTP POST to the adapter.
+        // The adapter will invoke the bot.
+        await _adapter.ProcessAsync(Request, Response, _bot, default(CancellationToken));
+    }
+}
